8c4a56798c9301629f0f6a76f145d8095fbc1919
Haydn <haydnmey@gmail.com>
2022-11-15 12:47:52 +0200

    removed packages and edited the sequelizerc file

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const indexRouter = express.Router();
-
-indexRouter.get("/", (req, res) => {
-  res.status(200).send("Hello can you hear me?");
-});
-
-export default indexRouter;
