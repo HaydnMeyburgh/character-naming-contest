c72427b76a1753fb8efed8e8f4bfeaf870a04984
Haydn <haydnmey@gmail.com>
2022-11-16 17:06:36 +0200

    created characters model and added associations into the static method

--- db/models/characters.js
+++ db/models/characters.js
@@ -2,24 +2,18 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Characters extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Characters.belongsTo(models.Character_Images);
+      Characters.belongsTo(models.Users);
     }
   }
   Characters.init(
     {
-      id: DataTypes.INTEGER,
       character_names: DataTypes.STRING,
-      photo_id: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "characters",
+      modelName: "Characters",
     }
   );
   return Characters;
