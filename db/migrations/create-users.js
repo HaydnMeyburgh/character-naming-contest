102f1f9a4fbd0b7d0b186f64e23d75fdf0a73ffa
Haydn <haydnmey@gmail.com>
2022-11-16 17:29:35 +0200

    edited column attributes and added constraints as well as validation for email and password

---
+++ db/migrations/create-users.js
@@ -0,0 +1,43 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        isEmail: true,
+      },
+      password: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+        validate: {
+          is: /^[0-9a-f]{64}$/i,
+        },
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
