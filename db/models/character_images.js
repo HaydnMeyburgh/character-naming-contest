8efdea8cf0f2b688fc506439aa327dec20ff77c8
Haydn <haydnmey@gmail.com>
2022-11-16 17:32:40 +0200

    Created the character images model and created the association to characters model

---
+++ db/models/character_images.js
@@ -0,0 +1,20 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Character_Images extends Model {
+    static associate(models) {
+      Character_Images.hasMany(models.Characters);
+    }
+  }
+  Character_Images.init(
+    {
+      name: DataTypes.STRING,
+      image_url: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: "Character_Images",
+    }
+  );
+  return Character_Images;
+};
