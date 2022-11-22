dcaec4ef5b093e9feec1669f74e93cf92621d78d
Haydn <haydnmey@gmail.com>
2022-11-22 06:46:12 +0200

    Restructured code to send seed as an actual bulk import

--- db/seeders/character-images.js
+++ db/seeders/character-images.js
@@ -10,36 +10,26 @@ module.exports = {
         image_url:
           "https://drive.google.com/uc?export=view&id=1gOaSw_IicT6tBrW03WpLzXmMcNQV6PLl",
       },
-    ]);
-    await queryInterface.bulkInsert("Character_Images", [
       {
         name: "Character 2",
         image_url:
           "https://drive.google.com/uc?export=view&id=14YMy4HF_wJ04J9EntAZfn-z3J56uDUlf",
       },
-    ]);
-    await queryInterface.bulkInsert("Character_Images", [
       {
         name: "Character 3",
         image_url:
           "https://drive.google.com/uc?export=view&id=1F5lA1OiNMyX3q5H9wdN9ifeLRTJ49bVQ",
       },
-    ]);
-    await queryInterface.bulkInsert("Character_Images", [
       {
         name: "Character 4",
         image_url:
           "https://drive.google.com/uc?export=view&id=1XczAzUJQmLc-kbeiB-AvB-G7oszl9v_J",
       },
-    ]);
-    await queryInterface.bulkInsert("Character_Images", [
       {
         name: "Character 5",
         image_url:
           "https://drive.google.com/uc?export=view&id=1nE1m8GjEqh7JHEQ_wf_OfazZB2rciotX",
       },
-    ]);
-    await queryInterface.bulkInsert("Character_Images", [
       {
         name: "Character 6",
         image_url:
@@ -49,6 +39,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('Character_Images', null, {});
+    await queryInterface.bulkDelete("Character_Images", null, {});
   },
 };
