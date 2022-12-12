8c19a592feb3b03df69fc5e7c86cd07c51a8339e
Haydn <haydnmey@gmail.com>
2022-12-12 11:33:15 +0200

    required in the auth middleware

--- routes/characters.js
+++ routes/characters.js
@@ -5,11 +5,12 @@ const {
   getCharacterById,
 } = require("../controllers/characters.controllers.js");
 const { createName } = require("../controllers/names.controllers.js");
+const auth = require("../middleware/auth");
 
 characterRouter.get("/", getCharacters);
 
 characterRouter.get("/:characterId", getCharacterById);
 
-characterRouter.post("/:characterId/name", createName);
+characterRouter.post("/:characterId/name", auth, createName);
 
 module.exports = characterRouter;
