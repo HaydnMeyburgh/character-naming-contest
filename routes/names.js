e5945fbacf954c44b10958b40dd64f96639e1fc7
Haydn <haydnmey@gmail.com>
2022-12-12 11:35:23 +0200

    removed updateName route

--- routes/names.js
+++ routes/names.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const namesRouter = express.Router();
-const { updateName, deleteName } = require("../controllers/names.controllers");
+const { deleteName } = require("../controllers/names.controllers");
 const auth = require("../middleware/auth");
-
-namesRouter.put("/:nameId", auth, updateName);
 
 namesRouter.delete("/:nameId", auth, deleteName);
 
