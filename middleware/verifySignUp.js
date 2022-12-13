2dcb8dc65c7f3f46537750e00389d9b9ddbdb4ab
Haydn <haydnmey@gmail.com>
2022-12-13 11:44:35 +0200

    Created singup verify middleware

---
+++ middleware/verifySignUp.js
@@ -0,0 +1,39 @@
+const Users = require("../models").Users;
+
+const checkDuplicateUsernameOrEmail = async (req, res, next) => {
+  const { username, email } = req.body;
+  try {
+    let user = await Users.findOne({
+      where: {
+        username: username,
+      },
+    });
+    if (user) {
+      return res.status(400).send({
+        message: "Failed! Username is already in use",
+      });
+    }
+    user = await Users.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (user) {
+      return res.status(400).send({
+        message: "Failed! Email is already in use!",
+      });
+    }
+    if (username === "" || email === "") {
+      return res.status(400).send({
+        message: "Username and/or email cannot be blank!",
+      });
+    }
+    next();
+  } catch (err) {
+    return res.status(500).send({
+      message: "unable to validate Username or email!",
+    });
+  }
+};
+
+module.exports = checkDuplicateUsernameOrEmail;
