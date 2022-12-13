e2f379ff2fd001e6d8ae180f3c6dff09149f8ec2
Haydn <haydnmey@gmail.com>
2022-12-13 11:45:40 +0200

    Addded logout function to terminate session

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -2,46 +2,24 @@ const jwt = require("jsonwebtoken");
 const Users = require("../models").Users;
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+require("dotenv").config();
 
 const signUp = async (req, res) => {
   const { firstName, lastName, username, email, password } = req.body;
   try {
-    // Check if email is already in use
-    let userExists = await Users.findOne({
-      where: {
-        email: email,
-      },
-    });
-    if (userExists) {
-      return res.status(401).send({
-        message: "This email is already in use",
-      });
-    }
-    // Check if email or password is blank
-    if (username === "" || password === "") {
-      return res.status(400).send({
-        message: "Username and Password cannot be blank",
-      });
-    }
     // Hash password and create user
-    bcrypt.hash(password, saltRounds, async function (err, hash) {
-      const newUser = await Users.create({
-        firstName: firstName,
-        lastName: lastName,
-        username: username,
-        email: email,
-        password: hash,
-      });
-      if (!newUser) {
-        return res.status(400).send({
-          message: "User could not be created",
-        });
-      }
-      res.status(201).send({
-        status: "Success",
-        data: newUser,
-      });
+    const newUser = await Users.create({
+      firstName: firstName,
+      lastName: lastName,
+      username: username,
+      email: email,
+      password: bcrypt.hashSync(password, saltRounds),
     });
+    if (newUser) {
+      return res.status(201).send({
+        message: "User registered successfully!",
+      });
+    }
   } catch (err) {
     return res.status(500).send({
       message: err.message,
@@ -50,32 +28,34 @@ const signUp = async (req, res) => {
 };
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { username, password, email } = req.body;
   try {
     // Checking to see if user exists
     const user = await Users.findOne({
       where: {
-        email: email,
+        username: username,
       },
     });
     if (!user) {
       return res.status(404).send({
-        message: "Invalid Credentials",
+        message: "Invalid Credentials, user not found",
       });
     }
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return res.status(404).send({
-        message: "Incorrect Password or email",
+    const validPassword = bcrypt.compareSync(password, user.password);
+    if (!validPassword) {
+      return res.status(401).send({
+        message: "Incorrect Password",
       });
     }
-    const token = jwt.sign({ userId: user.id }, "Random_secret", {
-      expiresIn: "24h",
+    const token = jwt.sign({ id: user.id }, process.env.JWTSECRET, {
+      expiresIn: 86400,
     });
+    req.session.token = token;
     res.status(202).send({
       message: "Successfully Logged in",
-      userId: user.id,
-      token: token,
+      id: user.id,
+      username: username,
+      email: email,
     });
   } catch (err) {
     return res.status(500).send({
@@ -83,22 +63,16 @@ const loginUser = async (req, res) => {
     });
   }
 };
-// Need to figure out the logout endpoint code
+
 const logoutUser = async (req, res) => {
   try {
-    if (req.session) {
-      req.session.destroy((err) => {
-        if (err) {
-          res.status(400).send({
-            message: "Unable to log out",
-          });
-        }
-        res.status(200).send({
-          message: "Logout successful",
-        });
-      });
-    }
-  } catch (err) {}
+    req.session = null;
+    return res.status(200).send({
+      mesage: "You've been signed out!",
+    });
+  } catch (err) {
+    this.next(err);
+  }
 };
 
 module.exports = {
