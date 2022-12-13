7f04131c0ab271e6c2a19ad45273a91120a3c123
Haydn <haydnmey@gmail.com>
2022-12-13 11:46:14 +0200

    adhusted the JWT auth middleware creation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,23 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const Users = require("../models").Users;
 
 const auth = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decodedToken = jwt.verify(token, "Random_secret");
-    const userId = decodedToken.userId;
-    req.auth = { userId };
-    if (req.body.userId && req.body.userId !== userId) {
-      throw "Invalid user ID";
-    } else {
-      next();
-    }
-  } catch {
-    res.status(401).send({
-      error: new Error("Invalid Request!"),
+  let token = req.session.token;
+  if (!token) {
+    return res.status(403).send({
+      message: "No token provided",
     });
   }
+  jwt.verify(token, process.env.JWTSECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({
+        message: "Unauthorised!",
+      });
+    }
+    req.userId = decoded.id;
+    next();
+  });
 };
 
 module.exports = auth;
