8bc7baa99d2fe2eaa82e91d57c3dbf69c62fe891
Haydn <haydnmey@gmail.com>
2022-12-13 12:02:34 +0200

    Added JWT auth to name delete route

--- routes/names.js
+++ routes/names.js
@@ -3,6 +3,11 @@ const namesRouter = express.Router();
 const { deleteName } = require("../controllers/names.controllers");
 const auth = require("../middleware/auth");
 
+namesRouter.use(function (req, res, next) {
+  req.header("Access-Control-Allow-Headers", "origin, Content-Type, Accept");
+  next();
+});
+
 namesRouter.delete("/:nameId", auth, deleteName);
 
 module.exports = namesRouter;
