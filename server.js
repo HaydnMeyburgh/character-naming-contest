a3996207f6954aeb4a1ff4fdf54c088153b9000e
Haydn <haydnmey@gmail.com>
2022-12-13 11:47:12 +0200

    created cookie-session to be used

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
-const env = require("dotenv");
-env.config();
+const env = require("dotenv").config();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
+const cookieSession = require("cookie-session");
 const characterRouter = require("./routes/characters");
 const namesRouter = require("./routes/names");
 const PORT = process.env.DEV_PORT || 3000;
 const usersRouter = require("./routes/user");
 
 app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cookieSession({
+    name: "character-name-session",
+    secret: process.env.COOKIESECRET,
+    httpOnly: true,
+  })
+);
 
 app.use(morgan("dev"));
 
