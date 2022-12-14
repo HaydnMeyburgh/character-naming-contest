7920147af85cedfbf08c7c7c5b4781b84bff48ac
Haydn <haydnmey@gmail.com>
2022-12-14 12:57:10 +0200

    refactored the delete endpoint

--- routes/names.js
+++ routes/names.js
@@ -3,11 +3,6 @@ const namesRouter = express.Router();
 const { deleteName } = require("../controllers/names.controllers");
 const auth = require("../middleware/auth");
 
-namesRouter.use(function (req, res, next) {
-  req.header("Access-Control-Allow-Headers", "origin, Content-Type, Accept");
-  next();
-});
-
 namesRouter.delete("/:nameId", auth, deleteName);
 
 module.exports = namesRouter;
