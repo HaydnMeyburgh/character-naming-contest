64845db31a606c4f4c456153c061323a544b2d47
Haydn <haydnmey@gmail.com>
2023-04-14 13:17:31 +0200

    implemented caching of images on first request

--- controllers/characters.controllers.js
+++ controllers/characters.controllers.js
@@ -1,24 +1,33 @@
 const Character_Images = require("../models").Character_Images;
 const Characters = require("../models").Characters;
+const NodeCache = require("node-cache");
+
+const myCache = new NodeCache({
+  stdTTL: 300,
+});
 
 const getCharacters = async (req, res) => {
-  try {
-    const allCharacterImages = await Character_Images.findAll({
-      attributes: ["image_url", "id"],
-    });
-
-    if (!allCharacterImages) {
-      return res.status(404).send({
-        message: "Images could not be retrieved",
+  if (myCache.has("char_images")) {
+    res.status(200).send({ images: myCache.get("char_images") });
+  } else {
+    try {
+      const allCharacterImages = await Character_Images.findAll({
+        attributes: ["image_url", "id"],
+      });
+      if (!allCharacterImages) {
+        return res.status(404).send({
+          message: "Images could not be retrieved",
+        });
+      }
+      myCache.set("char_images", allCharacterImages);
+      res.status(200).send({
+        images: allCharacterImages,
+      });
+    } catch (err) {
+      res.status(500).send({
+        error: err.message,
       });
     }
-    res.status(200).send({
-      images: allCharacterImages,
-    });
-  } catch (err) {
-    res.status(500).send({
-      error: err.message,
-    });
   }
 };
 
