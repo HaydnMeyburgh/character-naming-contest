522a1d55162836f1a05445c22fa1f8ea55768024
Haydn <haydnmey@gmail.com>
2023-05-23 18:41:02 +0200

    fixed route url

--- routes/votes.js
+++ routes/votes.js
@@ -4,7 +4,7 @@ const { castVote, deleteVote } = require("../controllers/votes.controllers.js");
 const auth = require("../middleware/auth");
 
 // Post a vote to the vote database
-voteRouter.post("name/:nameId/", auth, castVote);
+voteRouter.post("/name/:nameId/", auth, castVote);
 
 // Remove a vote
 voteRouter.delete("/:voteId", auth, deleteVote);
